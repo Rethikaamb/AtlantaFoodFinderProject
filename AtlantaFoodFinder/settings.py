9fc06acee8642a22626440214a12d4faa034af43
Enya Perry <enyaperry@lawn-143-215-124-49.lawn.gatech.edu>
2024-09-24 14:53:22 -0400

    commit

--- AtlantaFoodFinder/settings.py
+++ AtlantaFoodFinder/settings.py
@@ -15,7 +15,7 @@ from . info import *
 import os
 
 #Google Maps API
-GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY', 'INSERT_APIKEY_HERE')
+GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY', 'INSERT_KEY_HERE')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
