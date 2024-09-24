e5a77027bf96d8b141848ccc3b1f99253df96f6e
Rishika Aila <rishikaa@gmail.com>
2024-09-24 14:40:29 -0400

    search update

--- AtlantaFoodFinder/settings.py
+++ AtlantaFoodFinder/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from .info import *
+from . info import *
 import os
 
 #Google Maps API
-GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY', 'INSERT_APIKEY_HERE')
+GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY', 'AIzaSyDTGnXo9k2G6SPUlSRXO2XFKpfdLsCcERk')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
