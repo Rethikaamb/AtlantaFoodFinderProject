f6cc1f4148cbe041f6dc985543184a9942b5ff40
eperry40 <enyaperry@gmail.com>
2024-09-25 14:54:02 -0400

    signup enhancements

--- AtlantaFoodFinder/settings.py
+++ AtlantaFoodFinder/settings.py
@@ -15,7 +15,7 @@ from . info import *
 import os
 
 #Google Maps API
-GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY', 'AIzaSyDTGnXo9k2G6SPUlSRXO2XFKpfdLsCcERk')
+GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY', '')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
