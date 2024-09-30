83d3a41fe2d4fe7ffd199641f9a98591d126df69
Vaishnavi Bhalla <Navibhalla113@gmail.com>
2024-09-30 19:23:28 -0400

    push map functionality with filters(Distance and Rating)

--- home/forms.py
+++ home/forms.py
@@ -1,14 +1,26 @@
 from django import forms
 
 class RestaurantSearchForm(forms.Form):
-    query = forms.CharField(label='Search for restaurants', max_length=100)
+    query = forms.CharField(
+        label='Search for restaurants',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
     rating = forms.ChoiceField(
         label='Minimum Rating',
-        choices=[(0, 'Any'), (1, '1+'), (2, '2+'), (3, '3+'), (4, '4+'), (5, '5')],
-        required = False
+        choices=[('0', 'Any'), ('1', '1+'), ('2', '2+'), ('3', '3+'), ('4', '4+'), ('5', '5')],
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-control'})
     )
-    distance=forms.ChoiceField(
+    distance = forms.ChoiceField(
         label='Maximum Distance',
-        choices=[(1609,'1 mile'), (4828, '3 miles'), (8046, '5 miles'), (16093, '10 miles'), (20233, '25 miles')],
-        required = False
+        choices=[
+            ('1609', '1 mile'),
+            ('4828', '3 miles'),
+            ('8046', '5 miles'),
+            ('16093', '10 miles'),
+            ('20233', '25 miles')
+        ],
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-control'})
     )
